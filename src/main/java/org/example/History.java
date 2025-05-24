8187103d748729727ad9ee399ed8b05dad6e523d
Bia-Lara <agiampedro@gmail.com>
2025-05-24 15:59:38 -0300

    refactor: desacoplamento da classe BankAccount em History

--- src/main/java/org/example/History.java
+++ src/main/java/org/example/History.java
@@ -6,16 +6,16 @@ import java.util.Stack;
 public class History {
     private final Stack<IMemento> history;
 
-    public History(BankAccount account) {
+    public History(IBankAccount account) {
         this.history = new Stack<IMemento>();
         history.push(account.saveState());
     }
 
-    public void save(BankAccount account) {
+    public void save(IBankAccount account) {
         history.push(account.saveState());
     }
 
-    public void undoTransference(BankAccount account) {
+    public void undoTransference(IBankAccount account) {
         if(history.size() <= 1) System.out.println("não há estado anterior");
         else{
             IMemento previousState = history.pop();
--- src/main/java/org/example/IBankAccount.java
+++ src/main/java/org/example/IBankAccount.java
@@ -2,4 +2,5 @@ package org.example;
 
 public interface IBankAccount {
     IMemento saveState();
+    void backState(IMemento memento);
 }
